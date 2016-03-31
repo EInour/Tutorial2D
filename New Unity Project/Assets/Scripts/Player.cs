65171b3ab4e787bd7b5e17e1c30e7206e287f884
EInour <nilsson.o.90@gmail.com>
2016-03-31 15:55:38 +0200

    Added Project Tutorial
    Created from tutorial step 1 and 2.
    https://www.youtube.com/watch?v=oK_NzdVSxaQ

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+
+    public float speed;
+    public float jumpPower;
+
+    public bool grounded;
+
+    private Rigidbody2D rbPlayer;
+	void Start ()
+    {
+        speed = 50f;
+        jumpPower = 150f;
+
+        rbPlayer = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("horizontal");
+
+        rbPlayer.AddForce((Vector2.right * speed) * h);
+    }
+}
