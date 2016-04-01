4bee2847fcefd2b1f6276cc92dac4b092895e518
EInour <nilsson.o.90@gmail.com>
2016-04-01 11:48:43 +0200

    Update video 3 and 4

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -3,30 +3,66 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-
+    public float maxSpeed;
     public float speed;
     public float jumpPower;
 
     public bool grounded;
+    private bool jumpState;
+    private bool oldJumpState;
 
     private Rigidbody2D rbPlayer;
+    private Animator animator;
+
 	void Start ()
     {
+        maxSpeed = 3f;
         speed = 50f;
-        jumpPower = 150f;
+        jumpPower = 250f;
 
         rbPlayer = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
 	}
 	
 	void Update ()
     {
-	
+
+        animator.SetBool("Grounded", grounded);
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        if (Input.GetAxis("Horizontal") < -0.1f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if (Input.GetAxis("Horizontal") > 0.1f)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        oldJumpState = jumpState;
+        jumpState = Input.GetButton("Jump");
+
+        if (jumpState && !oldJumpState && grounded)
+        {
+            rbPlayer.AddForce(Vector2.up * jumpPower);
+        }
 	}
 
     void FixedUpdate()
     {
-        float h = Input.GetAxis("horizontal");
+        float h = Input.GetAxis("Horizontal");
 
         rbPlayer.AddForce((Vector2.right * speed) * h);
+
+        if (rbPlayer.velocity.x > maxSpeed)
+        {
+            rbPlayer.velocity = new Vector2(maxSpeed, rbPlayer.velocity.y);
+        }
+
+        else if (rbPlayer.velocity.x < -maxSpeed)
+        {
+            rbPlayer.velocity = new Vector2(-maxSpeed, rbPlayer.velocity.y);
+        }
     }
 }
