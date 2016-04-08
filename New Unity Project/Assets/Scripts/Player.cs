075afae1b793c0e4c4b32c5fdb46b425080b3fd4
Oscar <oscar_pleininger@hotmail.com>
2016-04-08 13:11:24 +0200

    Bullet update

--- New Unity Project/Assets/Scripts/Player.cs
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -6,27 +6,53 @@ public class Player : MonoBehaviour {
     public float maxSpeed;
     public float speed;
     public float jumpPower;
+    public float speedBoostTimer = 0;
 
     public bool grounded;
+    public int doubleJump;
+    private bool HasDoubleJumped;
     private bool jumpState;
     private bool oldJumpState;
+    public bool knockedBack;
+    public bool autoAimed;
 
     private Rigidbody2D rbPlayer;
+    public Player2 player2;
     private Animator animator;
 
-	void Start ()
+
+    public Transform firePoint;
+    public GameObject laserBullet;
+
+
+    void Start()
     {
         maxSpeed = 6f;
         speed = 50f;
         jumpPower = 250f;
 
+        doubleJump = 0;
+
         rbPlayer = gameObject.GetComponent<Rigidbody2D>();
 
         animator = gameObject.GetComponent<Animator>();
-	}
-	
-	void Update ()
+
+        player2 = player2.GetComponent<Player2>();
+    }
+
+    void Update()
     {
+
+        if (speedBoostTimer > 0)
+        {
+            speedBoostTimer -= Time.deltaTime;
+            maxSpeed = 10f;
+        }
+
+        if (speedBoostTimer <= 0)
+        {
+            maxSpeed = 6f;
+        }
 
         animator.SetBool("Grounded", grounded);
         animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
@@ -44,14 +70,49 @@ public class Player : MonoBehaviour {
         oldJumpState = jumpState;
         jumpState = Input.GetButton("Jump");
 
+
+        //DoubleJump
+        if (jumpState && !oldJumpState && !grounded && (doubleJump > 0) && !HasDoubleJumped)
+        {
+            HasDoubleJumped = true;
+
+            rbPlayer.AddForce(Vector2.up * jumpPower);
+
+            doubleJump -= 1;
+        }
+
+
+        //SingleJump
         if (jumpState && !oldJumpState && grounded)
         {
+            HasDoubleJumped = false;
+
+            rbPlayer.velocity = new Vector2(rbPlayer.velocity.x, 0);
+
             rbPlayer.AddForce(Vector2.up * jumpPower);
         }
-	}
+
+        //Shoot
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(laserBullet, firePoint.position, firePoint.rotation);
+        }
+
+        if (player2.autoAimed)
+        {
+            Instantiate(laserBullet, firePoint.position, firePoint.rotation);
+            player2.autoAimed = false;
+        }
+    }
 
     void FixedUpdate()
     {
+        if (knockedBack)
+        {
+            rbPlayer.AddForce(Vector2.left * speed);
+            knockedBack = false;
+        }
+
         float h = Input.GetAxis("Horizontal");
 
         rbPlayer.AddForce((Vector2.right * speed) * h);
@@ -66,4 +127,20 @@ public class Player : MonoBehaviour {
             rbPlayer.velocity = new Vector2(-maxSpeed, rbPlayer.velocity.y);
         }
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DoubleJump"))
+        {
+            other.gameObject.SetActive(false);
+            doubleJump += 1;
+        }
+
+        else if (other.gameObject.CompareTag("SpeedBoost"))
+        {
+            other.gameObject.SetActive(false);
+            speedBoostTimer = 2;
+        }
+    }
+
 }
--- New Unity Project/Assets/Scripts/Player2.cs
+++ New Unity Project/Assets/Scripts/Player2.cs
@@ -7,13 +7,22 @@ public class Player2 : MonoBehaviour
     public float maxSpeed;
     public float speed;
     public float jumpPower;
+    public float speedBoostTimer = 0;
 
     public bool grounded;
+    public int doubleJump;
+    private bool HasDoubleJumped;
     private bool jumpState;
     private bool oldJumpState;
+    public bool knockedBack;
+    public bool autoAimed;
 
     private Rigidbody2D rbPlayer;
+    public Player player1;
     private Animator animator;
+
+    public Transform firePoint;
+    public GameObject laserBullet;
 
     void Start()
     {
@@ -21,13 +30,29 @@ public class Player2 : MonoBehaviour
         speed = 50f;
         jumpPower = 250f;
 
+        doubleJump = 0;
+
         rbPlayer = gameObject.GetComponent<Rigidbody2D>();
 
         animator = gameObject.GetComponent<Animator>();
+
+        player1 = player1.GetComponent<Player>();
     }
 
     void Update()
     {
+        
+
+        if (speedBoostTimer > 0)
+        {
+            speedBoostTimer -= Time.deltaTime;
+            maxSpeed = 10f;
+        }
+
+        if (speedBoostTimer <= 0)
+        {
+            maxSpeed = 6f;
+        }
 
         animator.SetBool("Grounded", grounded);
         animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal2")));
@@ -45,14 +70,47 @@ public class Player2 : MonoBehaviour
         oldJumpState = jumpState;
         jumpState = Input.GetButton("Jump2");
 
+
+        //DoubleJump
+        if (jumpState && !oldJumpState && !grounded && (doubleJump > 0) && !HasDoubleJumped)
+        {
+            HasDoubleJumped = true;
+
+            rbPlayer.AddForce(Vector2.up * jumpPower);
+
+            doubleJump -= 1;
+        }
+
+
+        //SingleJump
         if (jumpState && !oldJumpState && grounded)
         {
+            HasDoubleJumped = false;
+
             rbPlayer.AddForce(Vector2.up * jumpPower);
+        }
+
+        //Shoot
+        if (Input.GetButtonDown("Fire2"))
+        {
+            Instantiate(laserBullet, firePoint.position, firePoint.rotation);
+        }
+
+        if (player1.autoAimed)
+        {
+            Instantiate(laserBullet, firePoint.position, firePoint.rotation);
+            player1.autoAimed = false;
         }
     }
 
     void FixedUpdate()
     {
+        if (knockedBack)
+        {
+            rbPlayer.AddForce(Vector2.left * speed);
+            knockedBack = false;
+        }
+
         float h = Input.GetAxis("Horizontal2");
 
         rbPlayer.AddForce((Vector2.right * speed) * h);
@@ -65,6 +123,21 @@ public class Player2 : MonoBehaviour
         else if (rbPlayer.velocity.x < -maxSpeed)
         {
             rbPlayer.velocity = new Vector2(-maxSpeed, rbPlayer.velocity.y);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DoubleJump"))
+        {
+            other.gameObject.SetActive(false);
+            doubleJump += 1;
+        }
+
+        else if (other.gameObject.CompareTag("SpeedBoost"))
+        {
+            other.gameObject.SetActive(false);
+            speedBoostTimer = 2;
         }
     }
 }
